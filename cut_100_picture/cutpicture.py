c461a64020be48e2bf7fc421fdaab575a9effdd5
Hien <hienbn3333@gmail.com>
2021-11-17 21:18:27 +0700

    full

---
+++ cut_100_picture/cutpicture.py
@@ -0,0 +1,77 @@
+import cv2
+import numpy as np
+
+# Load HAAR face classifier
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Load functions
+def face_extractor(img):
+    columns = ['SERIAL NO.', '', 'ID', '', 'NAME']
+    assure_path_exists("StudentDetails/")
+    assure_path_exists("TrainingImage/")
+    serial = 0
+    exists = os.path.isfile("StudentDetails\StudentDetails.csv")
+    if exists:
+        with open("StudentDetails\StudentDetails.csv", 'r') as csvFile1:
+            reader1 = csv.reader(csvFile1)
+            for l in reader1:
+                serial = serial + 1
+        serial = (serial // 2)
+        csvFile1.close()
+    else:
+        with open("StudentDetails\StudentDetails.csv", 'a+') as csvFile1:
+            writer = csv.writer(csvFile1)
+            writer.writerow(columns)
+            serial = 1
+        csvFile1.close()
+    Id = (txt.get()) # Lấy ID từ ô nhập
+    name = (txt2.get()) # Lấy name từ ô nhập
+
+    # Function detects faces and returns the cropped face
+    # If no face detected, it returns the input image
+    
+    #gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(img, 1.3, 5)
+    
+    if faces is ():
+        return None
+    
+    # Crop all faces found
+    for (x,y,w,h) in faces:
+        x=x-10
+        y=y-10
+        cropped_face = img[y:y+h+50, x:x+w+50]
+
+    return cropped_face
+
+# Initialize Webcam
+cap = cv2.VideoCapture(0)
+count = 0
+
+# Collect 100 samples of your face from webcam input
+while True:
+
+    ret, frame = cap.read()
+    if face_extractor(frame) is not None:
+        count += 1
+        face = cv2.resize(face_extractor(frame), (400, 400))
+        #face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+        # Save file in specified directory with unique name
+        file_name_path = './Image/' + str(count) + '.jpg'
+        cv2.imwrite(file_name_path, face)
+
+        # Put count on images and display live count
+        cv2.putText(face, str(count), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+        cv2.imshow('Face Cropper', face)
+        
+    else:
+        print("Face not found")
+        pass
+
+    if cv2.waitKey(1) == 13 or count == 100: #13 is the Enter Key
+        break
+        
+cap.release()
+cv2.destroyAllWindows()      
+print("Collecting Samples Complete")
